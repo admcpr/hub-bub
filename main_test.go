3cad618abb7e87752d0d524346d33819e35a0fd0
adam <hello@admcpr.com>
2022-11-28 14:46:49 +0100

    Closes #1

---
+++ main_test.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestYesNo(t *testing.T) {
+	type args struct {
+		b bool
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		// TODO: Add test cases.
+		{"Yes", args{true}, "Yes"},
+		{"No", args{false}, "No"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := YesNo(tt.args.b); got != tt.want {
+				t.Errorf("YesNo() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
