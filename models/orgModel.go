3d209054657e9227b82d7ca01a5c8ce551d4c4c5
Adam Cooper <mail@admcpr.com>
2023-03-28 20:39:27 +0200

    Add repository name filtering (#32)
    * Pass all messages to repoList
    
    ---------
    
    Co-authored-by: Adam <hello@admcpr.com>

--- models/orgModel.go
+++ models/orgModel.go
@@ -93,20 +93,18 @@ func (m OrgModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 				_, cmd = m.repoModel.Update(msg)
 			}
 		} else {
-			switch msg.Type {
-			case tea.KeyDown, tea.KeyUp:
-				m.repoList, cmd = m.repoList.Update(msg)
-				m.repoModel.SelectRepo(m.getSelectedRepo(), half(m.width), m.height)
-			case tea.KeyEnter:
+			switch msg.String() {
+			case tea.KeyEnter.String():
 				m.tabsHaveFocus = true
-				m.repoModel.SelectRepo(m.getSelectedRepo(), half(m.width), m.height)
-			case tea.KeyEsc:
+			case tea.KeyEsc.String():
 				return MainModel[UserModelName], nil
+			case "ctrl+c", "q":
+				if m.repoList.FilterState() == list.Unfiltered {
+					return m, tea.Quit
+				}
 			}
-		}
-		switch msg.String() {
-		case "ctrl+c", "q":
-			return m, tea.Quit
+			m.repoList, cmd = m.repoList.Update(msg)
+			m.repoModel.SelectRepo(m.getSelectedRepo(), half(m.width), m.height)
 		}
 	}
 
