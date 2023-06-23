36d6a5f24fcf3c70dc0fe327ba9c7ecd0675c108
adam <hello@admcpr.com>
2023-06-23 16:38:12 +0200

    Number filter model tests

---
+++ models/filterNumberModel_test.go
@@ -0,0 +1,52 @@
+package models
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewFilterNumberModel(t *testing.T) {
+	tests := []struct {
+		title string
+		from  int
+		to    int
+	}{
+		{"Test 1", 0, 10},
+		{"Test 2", -5, 5},
+		{"Test 3", 100, 200},
+		{"Test 4", -100, 0},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.title, func(t *testing.T) {
+			m := NewFilterNumberModel(tt.title, tt.from, tt.to)
+
+			assert.Equal(t, m.Title, tt.title)
+			assert.Equal(t, m.fromInput.Placeholder, fmt.Sprint(tt.from))
+			assert.Equal(t, m.toInput.Placeholder, fmt.Sprint(tt.to))
+		})
+	}
+}
+
+func TestNumberValidator(t *testing.T) {
+	tests := []struct {
+		input  string
+		prompt string
+		want   error
+	}{
+		{"123", "Test 1", nil},
+		{"-5", "Test 2", nil},
+		{"abc", "Test 3", fmt.Errorf("please enter a number for the `Test 3` value")},
+		{"1.23", "Test 4", fmt.Errorf("please enter a number for the `Test 4` value")},
+	}
+
+	for _, tt := range tests {
+		t.Run(fmt.Sprintf("%s: %s", tt.prompt, tt.input), func(t *testing.T) {
+			got := numberValidator(tt.input, tt.prompt)
+
+			assert.Equal(t, got, tt.want, fmt.Sprintf("got %q, want %q", got, tt.want))
+		})
+	}
+}
